c706e2688b91415cddcf4872c93d45b86b36d506
Elton Aliyev <aliyevelton88@gmail.com>
2024-04-19 08:28:32 +0400

    first commit

---
+++ Riode/Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Riode.Areas.Admin.Controllers
+{
+	public class BrandController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
